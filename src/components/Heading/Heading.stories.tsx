215a0aec41b4d933d12bbba0dafde46d9ec141fd
victordev13 <vtrcarvalho.13@gmail.com>
2022-10-12 21:55:34 -0300

    feat: Heading component

--- src/components/Heading/Heading.stories.tsx
+++ src/components/Heading/Heading.stories.tsx
@@ -1,9 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Text, TextProps } from '.';
+import { Heading, HeadingProps } from '.';
 
 export default {
-  title: 'Components/Text',
-  component: Text,
+  title: 'Components/Heading',
+  component: Heading,
   args: {
     children: 'Hello World',
   },
@@ -15,32 +15,37 @@ export default {
       }
     }
   }
-} as Meta<TextProps>;
+} as Meta<HeadingProps>;
 
-export const Default: StoryObj<TextProps> = {};
+export const Default: StoryObj<HeadingProps> = {};
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj<HeadingProps> = {
   args: {
     size: 'sm',
   },
 };
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
   args: {
     size: 'lg',
   },
 };
 
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
   args: {
     asChild: true,
     children: (
-      <p>Testando</p>
+      <h1>Custom</h1>
     )
   },
   argTypes: {
     children: {
-      control: {
+      table: {
+        disable: true
+      }
+    },
+    asChild: {
+      table: {
         disable: true
       }
     }
